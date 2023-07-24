c2c2c9389f1a01c6bf6a874d1e20dc0271c847bb
Adam Stachowski <131054039+adstah@users.noreply.github.com>
2023-07-24 15:16:16 +0200

    update blink

--- blink/blink.ino
+++ blink/blink.ino
@@ -5,7 +5,7 @@ void setup() {
 
 void loop() {
   digitalWrite(LED_BUILTIN, LOW);  // Set output to LOW / 0
-  delay(500);                      // Stop the program for 1 second
+  delay(500);                      // Stop the program for .5 second
   digitalWrite(LED_BUILTIN, HIGH);  // Set output to HIGH / 1
-  delay(1000);                      // Stop the program for 2 seconds
+  delay(1000);                      // Stop the program for 1 second
 }
